d34310ff0ba87e418fe4b1249ea8488629472d62
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-06-26 09:35:19 +0200

    Tiles are now grabbable

--- src/tile.js
+++ src/tile.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader';
 import "./Tile.css";
 
 function Tile(props) {
+  const [grabbed, setGrabbed] = useGrab();
+  const mouseY = useMouseY();
+  const [initialY, setInitialY] = useState(0);
+
+  function handleGrab(event) {
+    event.preventDefault();
+    setGrabbed(true);
+    setInitialY(event.clientY);
+  }
+
+  var className = "tile";
+  if (grabbed) {
+    className += " grabbed";
+  }
+
+  var top = grabbed ? (mouseY - initialY) : 0;
+
   return (
-    <div className="tile" style={{backgroundColor: props.colour, width: "calc(" + props.nights + "00% + " + props.nights + "px)"}}>
+    <div className={className} onMouseDown={handleGrab} style={{top: top + "px", backgroundColor: props.colour, width: "calc(" + props.nights + "00% + " + (props.nights - 1) + "px)"}}>
       <span className="tile-name">{props.name}</span>
       <span className="tile-room">{props.roomType}</span>
     </div>
   );
+}
+
+function useGrab() {
+  const [grabbed, setGrabbed] = useState(false);
+  useEffect(() => {
+    function unsetGrab() {
+      setGrabbed(false);
+    }
+    window.addEventListener('mouseup', unsetGrab);
+    return () => window.removeEventListener('mouseup', unsetGrab);
+  }, []);
+  return [grabbed, setGrabbed];
+}
+
+function useMouseY() {
+  const [mouseY, setMouseY] = useState(0);
+  useEffect(() => {
+    function updateMouseY(event) {
+      setMouseY(event.clientY);
+    }
+    window.addEventListener('mousemove', updateMouseY);
+    return () => window.removeEventListener('mousemove', updateMouseY);
+  }, []);
+  return mouseY;
 }
 
 export default hot(module)(Tile);
