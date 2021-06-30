8858da06747112bd7a45d5e0d1730d04f1656b6c
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-06-30 15:13:53 +0200

    Implemented tile move via reducer

--- src/tile.js
+++ src/tile.js
@@ -1,55 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { hot } from 'react-hot-loader';
 import "./Tile.css";
 
 function Tile(props) {
-  const [grabbed, setGrabbed] = useGrab();
-  const mouseY = useMouseY();
-  const [initialY, setInitialY] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { grabbed: false, initialY: 0, top: 0 });
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "grab":
+        return { grabbed: true, initialY: action.event.pageY, top: 0 };
+      case "move":
+        return {
+          grabbed: state.grabbed,
+          initialY: state.initialY,
+          top: state.grabbed ? (action.event.pageY - state.initialY) : 0
+        };
+      case "drop":
+        return { grabbed: false, initialY: 0, top: 0 };
+      default:
+        break;
+    }
+  }
 
   function handleGrab(event) {
     event.preventDefault();
-    setGrabbed(true);
-    setInitialY(event.pageY);
+    dispatch({ type: "grab", event: event });
   }
 
+  useEffect(() => {
+    function drop() {
+      dispatch({ type: "drop" });
+    }
+    window.addEventListener('mouseup', drop);
+    return () => window.removeEventListener('mouseup', drop);
+  }, []);
+
+  useEffect(() => {
+    function move(event) {
+      dispatch({ type: "move", event: event });
+    }
+    window.addEventListener('mousemove', move);
+    return () => window.removeEventListener('mousemove', move);
+  }, []);
+
   var className = "tile";
-  if (grabbed) {
+  if (state.grabbed) {
     className += " grabbed";
   }
 
-  var top = grabbed ? (mouseY - initialY) : 0;
-
   return (
-    <div className={className} onMouseDown={handleGrab} style={{top: top + "px", backgroundColor: props.colour, width: "calc(" + props.nights + "00% + " + (props.nights - 1) + "px)"}}>
+    <div className={className} onMouseDown={handleGrab} style={{top: state.top + "px", backgroundColor: props.colour, width: "calc(" + props.nights + "00% + " + (props.nights - 1) + "px)"}}>
       <span className="tile-name">{props.name}</span>
       <span className="tile-room">{props.roomType}</span>
     </div>
   );
-}
-
-function useGrab() {
-  const [grabbed, setGrabbed] = useState(false);
-  useEffect(() => {
-    function unsetGrab() {
-      setGrabbed(false);
-    }
-    window.addEventListener('mouseup', unsetGrab);
-    return () => window.removeEventListener('mouseup', unsetGrab);
-  }, []);
-  return [grabbed, setGrabbed];
-}
-
-function useMouseY() {
-  const [mouseY, setMouseY] = useState(0);
-  useEffect(() => {
-    function updateMouseY(event) {
-      setMouseY(event.pageY);
-    }
-    window.addEventListener('mousemove', updateMouseY);
-    return () => window.removeEventListener('mousemove', updateMouseY);
-  }, []);
-  return mouseY;
 }
 
 export default hot(module)(Tile);
