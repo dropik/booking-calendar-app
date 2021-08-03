d4df3d4b559ac80a2131c2d7481743500955a16f
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-08-03 12:00:52 +0200

    Fixed crash on date clear

--- src/Header.js
+++ src/Header.js
@@ -4,11 +4,19 @@ import DatesContainer from './DatesContainer';
 import "./Header.css";
 
 function Header(props) {
+  function handleDateChange(event) {
+    if (!event.target.value) {
+      event.preventDefault();
+    } else {
+      props.onDateChange(event);
+    }
+  }
+
   return (
     <div className="header">
       <div className="data-input">
         <span>From: </span>
-        <input type="date" id="fromDate" defaultValue={props.date.toISOString().substr(0,10)} onChange={props.onDateChange}/>
+        <input type="date" id="fromDate" defaultValue={props.date.toISOString().substr(0,10)} onChange={handleDateChange} />
       </div>
       <DatesContainer date={props.date} columns={props.columns}/>
     </div>
