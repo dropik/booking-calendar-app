c6a150364f4f16ea6f697209ed8d60e9716ccc49
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-08-28 20:39:21 +0200

    Added store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+import store from './store';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
