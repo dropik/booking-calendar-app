afa6303ad7dcf9f2aa65c49cb55c0a7f6253e422
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-08-31 01:00:28 +0200

    Optimized dates update on scroll

--- src/Dates.js
+++ src/Dates.js
@@ -1,31 +1,28 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useMemo } from 'react';
 import { hot } from 'react-hot-loader';
 import Day from './Day';
+import { useSelector } from 'react-redux';
 import "./Dates.css";
 
 function Dates(props) {
-  const datesRef = useRef(null);
+  const scrollLeft = useSelector(state => state.horizontalScroll.scrollLeft);
+  var dates = useMemo(() => computeDates(props.startDate, props.columns), [props.startDate, props.columns]);
+  return (
+    <div style={{left: -scrollLeft + "px"}} className="dates">{dates}</div>
+  );
+}
 
-  useEffect(() => {
-    function handleScroll(event) {
-      datesRef.current.style.left = -event.target.scrollLeft + "px";
-    }
-    document.getElementById("tableContainer").addEventListener('scroll', handleScroll);
-    return () => document.getElementById("tableContainer").removeEventListener('scroll', handleScroll);
-  }, []);
+function computeDates(startDate, columns) {
+  var dates = [];
 
-  var columns = [];
-
-  var dateCounter = new Date(props.startDate.getTime());
-  for (var i = 0; i < props.columns; i++) {
+  var dateCounter = new Date(startDate.getTime());
+  for (var i = 0; i < columns; i++) {
     var day = dateCounter.getDate();
-
-    columns.push(<Day day={day.toString().padStart(2, '0')} key={dateCounter.toDateString()}/>);
-
+    dates.push(<Day day={day.toString().padStart(2, '0')} key={dateCounter.toDateString()}/>);
     dateCounter.setDate(dateCounter.getDate() + 1);
   }
 
-  return <div ref={datesRef} className="dates">{columns}</div>
+  return dates;
 }
 
 export default hot(module)(Dates);
