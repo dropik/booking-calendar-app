3aa2b8fa627714680a31bfd04986c45e3ca3256a
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-03-03 23:10:31 +0100

    Fixed unassigned tiles slice file name

--- src/redux/root.ts
+++ src/redux/root.ts
@@ -5,7 +5,7 @@ import hotel from "./hotelSlice";
 import scroll from "./scrollSlice";
 import tiles from "./tilesSlice";
 import assignedTiles from "./assignedTilesSlice";
-import unassignedTiles from "./unassignedTiles";
+import unassignedTiles from "./unassignedTilesSlice";
 import hoveredId from "./hoveredIdSlice";
 import roomTypes from "./roomTypesSlice";
 
