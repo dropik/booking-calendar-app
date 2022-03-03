8d7264098c5b51b9ec66fa4b79d5f363bd558644
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-03-03 18:52:54 +0100

    Designed unassigned tile alert on day cell

--- src/components/header/dates-container/Day.tsx
+++ src/components/header/dates-container/Day.tsx
@@ -1,5 +1,7 @@
 import React, { memo } from "react";
 import { hot } from "react-hot-loader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 import "./Day.css";
 
@@ -11,6 +13,7 @@ function Day(props: Props): JSX.Element {
   return (
     <div className="day">
       <b>{props.day}</b>
+      <span className="day-alert" title="Ci sono occupazioni non assegnati"><FontAwesomeIcon icon={faCircleExclamation} /></span>
     </div>
   );
 }
