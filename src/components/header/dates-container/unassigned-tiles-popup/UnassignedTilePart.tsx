b52ba5573a1e14259e40584b227c3e773ba9f8d0
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-03-07 09:09:48 +0100

    Showing occupation info for tiles in popup

--- src/components/header/dates-container/unassigned-tiles-popup/UnassignedTilePart.tsx
+++ src/components/header/dates-container/unassigned-tiles-popup/UnassignedTilePart.tsx
@@ -1,7 +1,10 @@
 import React, { useLayoutEffect, useRef } from "react";
 import { hot } from "react-hot-loader";
+import { AnyAction } from "@reduxjs/toolkit";
 
-import { useAppSelector } from "../../../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../../../redux/hooks";
+import * as TilesSlice from "../../../../redux/tilesSlice";
+import * as HoveredIdSlice from "../../../../redux/hoveredIdSlice";
 
 import "./UnassignedTilePart.css";
 
@@ -11,24 +14,49 @@ type Props = {
 };
 
 function UnassignedTilePart(props: Props): JSX.Element {
-  const tileData = useAppSelector((state) => state.tiles.data[props.tileId]);
+  const dispatch = useAppDispatch();
+  const tileData = useTileData(props.tileId);
   const ref = useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
-    if (ref.current) {
-      ref.current.style.backgroundColor = tileData.colour;
-    }
-  }, [ref, tileData]);
+  useBackgroundColorEffect(ref, tileData);
 
   if (!props.hasTilePart) {
     return <></>;
   }
 
+  const enterHandler = getEnterHandler(dispatch, props.tileId);
+  const leaveHandler = getLeaveHandler(dispatch);
+
   return (
-    <div ref={ref} className="unassigned-tile-part">
+    <div
+      ref={ref}
+      onMouseEnter={enterHandler}
+      onMouseLeave={leaveHandler}
+      className="unassigned-tile-part"
+    >
       <b>{tileData.persons}</b>
     </div>
   );
+}
+
+function useTileData(tileId: string): TilesSlice.TileData {
+  return useAppSelector((state) => state.tiles.data[tileId]);
+}
+
+function useBackgroundColorEffect(ref: React.RefObject<HTMLDivElement>, tileData: TilesSlice.TileData): void {
+  useLayoutEffect(() => {
+    if (ref.current) {
+      ref.current.style.backgroundColor = tileData.colour;
+    }
+  }, [ref, tileData]);
+}
+
+function getEnterHandler(dispatch: React.Dispatch<AnyAction>, tileId: string): () => void {
+  return () => dispatch(HoveredIdSlice.set(tileId));
+}
+
+function getLeaveHandler(dispatch: React.Dispatch<AnyAction>): () => void {
+  return () => dispatch(HoveredIdSlice.set(undefined));
 }
 
 export default hot(module)(UnassignedTilePart);
