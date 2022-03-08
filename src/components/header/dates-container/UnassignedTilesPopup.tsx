3e637dd42673e744ca62a735d5073dc1fd4722c3
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-03-08 01:53:03 +0100

    Splitted popup component

--- src/components/header/dates-container/UnassignedTilesPopup.tsx
+++ src/components/header/dates-container/UnassignedTilesPopup.tsx
@@ -1,89 +1,53 @@
-import React, { useEffect, useLayoutEffect, useMemo, useRef } from "react";
+import React, { useEffect } from "react";
 import { hot } from "react-hot-loader";
 import { AnyAction } from "@reduxjs/toolkit";
 
-import { useAppDispatch, useAppSelector, useLeftShift } from "../../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import * as Utils from "../../../utils";
 import * as TilesSlice from "../../../redux/tilesSlice";
 
-import UnassignedRow from "./unassigned-tiles-popup/UnassignedRow";
+import UnassignedTilesPopupBody from "./unassigned-tiles-popup/UnassignedTilesPopupBody";
 
-import "./UnassignedTilesPopup.css";
+type PopupData = {
+  show: boolean,
+  leftmostSelectedTileDate: string,
+  tilesPerSelectedDay: { [key: string]: string }
+}
 
 function UnassignedTilesPopup(): JSX.Element {
   const dispatch = useAppDispatch();
-  const show = useShowPopup();
-  const leftmostSelectedTileDate = useLeftmostSelectedTileDate();
-  const tilesPerSelectedDay = useTilesPerSelectedDay();
-  const left = useLeftShift(leftmostSelectedTileDate);
-  const ref = useRef<HTMLDivElement>(null);
+  const popupData = usePopupData();
 
-  const className = getClassName(show);
-  const rows = useRowsMemo(tilesPerSelectedDay, leftmostSelectedTileDate);
-
-  useScrollEffect(ref, left);
   useHideOnClickOutsidePopupEffect(dispatch);
 
-  return (<div ref={ref} className={className}>{rows}</div>);
+  if (popupData && popupData.show) {
+    return (
+      <UnassignedTilesPopupBody
+        leftmostSelectedTileDate={popupData.leftmostSelectedTileDate}
+        tilesPerSelectedDay={popupData.tilesPerSelectedDay}
+      />
+    );
+  }
+
+  return <></>;
 }
 
-function useShowPopup(): boolean | undefined {
-  return useAppSelector((state) => {
-    const selectedDate = state.tiles.selectedDate;
-    if (selectedDate && state.tiles.unassignedMap[selectedDate]) {
-      return (Object.keys(state.tiles.unassignedMap[selectedDate]).length > 0);
-    }
-  });
-}
-
-function useLeftmostSelectedTileDate(): string | undefined {
+function usePopupData(): PopupData | undefined {
   return useAppSelector((state) => {
     const selectedDate = state.tiles.selectedDate;
     if (selectedDate && state.tiles.unassignedMap[selectedDate]) {
       let leftmostSelectedTileDate = selectedDate;
+      let show = false;
       for (const tileId in state.tiles.unassignedMap[selectedDate]) {
+        show = true;
         const tile = state.tiles.data[tileId];
         if (Utils.daysBetweenDates(leftmostSelectedTileDate, tile.from) < 0) {
           leftmostSelectedTileDate = tile.from;
         }
       }
-      return leftmostSelectedTileDate;
+      return { show, leftmostSelectedTileDate, tilesPerSelectedDay: state.tiles.unassignedMap[selectedDate] };
     }
   });
-}
-
-function useTilesPerSelectedDay(): { [key: string]: string } {
-  return useAppSelector((state) => state.tiles.unassignedMap[state.tiles.selectedDate as string]);
-}
-
-function getClassName(show: boolean | undefined): string {
-  let className = "unassigned-tiles-popup";
-  if (!show) {
-    className += " hidden";
-  }
-  return className;
-}
-
-function useRowsMemo(tilesPerSelectedDay: { [key: string]: string }, leftmostSelectedTileDate: string | undefined): JSX.Element[] {
-  return useMemo(() => {
-    const rows: JSX.Element[] = [];
-    if (tilesPerSelectedDay) {
-      for (const tileId in tilesPerSelectedDay) {
-        if (leftmostSelectedTileDate) {
-          rows.push(<UnassignedRow key={tileId} tileId={tileId} leftmostSelectedTileDate={leftmostSelectedTileDate} />);
-        }
-      }
-    }
-    return rows;
-  }, [tilesPerSelectedDay, leftmostSelectedTileDate]);
-}
-
-function useScrollEffect(ref: React.RefObject<HTMLDivElement>, left: number): void {
-  useLayoutEffect(() => {
-    if (ref.current) {
-      ref.current.style.left = `${left}px`;
-    }
-  }, [ref, left]);
 }
 
 function useHideOnClickOutsidePopupEffect(dispatch: React.Dispatch<AnyAction>): void {
