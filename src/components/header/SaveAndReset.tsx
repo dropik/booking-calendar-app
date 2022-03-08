5a42a617ed31d629b6abfde42c262269c0371222
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-03-08 13:36:32 +0100

    Showing save and reset buttons only when chages present

--- src/components/header/SaveAndReset.tsx
+++ src/components/header/SaveAndReset.tsx
@@ -1,11 +1,19 @@
-import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { hot } from "react-hot-loader";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+
+import { useAppSelector } from "../../redux/hooks";
 
 import "./SaveAndReset.css";
 
 function SaveAndReset(): JSX.Element {
+  const hasChanges = useHasChanges();
+
+  if (!hasChanges) {
+    return <></>;
+  }
+
   return (
     <div className="save-and-reset">
       <div className="button reset">
@@ -14,6 +22,10 @@ function SaveAndReset(): JSX.Element {
       <div className="button save">Salva</div>
     </div>
   );
+}
+
+function useHasChanges(): boolean {
+  return useAppSelector((state) => Object.keys(state.tiles.changesMap).length > 0);
 }
 
 export default hot(module)(SaveAndReset);
