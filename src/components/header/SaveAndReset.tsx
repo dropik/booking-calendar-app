9c00d545e134939a3df5aaf4594e63e2de914a14
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-03-08 13:31:43 +0100

    Reset button as icon

--- src/components/header/SaveAndReset.tsx
+++ src/components/header/SaveAndReset.tsx
@@ -1,3 +1,5 @@
+import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { hot } from "react-hot-loader";
 
@@ -6,7 +8,9 @@ import "./SaveAndReset.css";
 function SaveAndReset(): JSX.Element {
   return (
     <div className="save-and-reset">
-      <div className="button reset">Reset</div>
+      <div className="button reset">
+        <FontAwesomeIcon icon={faRotateLeft} />
+      </div>
       <div className="button save">Salva</div>
     </div>
   );
