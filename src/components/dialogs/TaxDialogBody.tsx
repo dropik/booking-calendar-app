13db4cfea3bd1351f5a3b250e44c2c1d099ed2ea
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-04-13 13:39:53 +0200

    Refactored TaxDialogBody

--- src/components/dialogs/TaxDialogBody.tsx
+++ src/components/dialogs/TaxDialogBody.tsx
@@ -1,8 +1,5 @@
 import React, { useContext, useState } from "react";
 import { hot } from "react-hot-loader";
-import DatePicker from "react-datepicker";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 import { useAppDispatch, useCurrentDate } from "../../redux/hooks";
 import * as ConnectionErrorSlice from "../../redux/connectionErrorSlice";
@@ -10,8 +7,13 @@ import * as Api from "../../api";
 import * as Utils from "../../utils";
 
 import { DialogContainerContext } from "./DialogContainer";
+import ErrorLabel from "./ErrorLabel";
+import LabeledDateInput from "./LabeledDateInput";
+import ButtonInput from "./ButtonInput";
+import DescriptionRow from "./DescriptionRow";
+import LoadingDataWrapper from "./LoadingDataWrapper";
 
-type DialogState = "fill" | "loading" | "done";
+type DialogState = "fill" | "loading";
 
 function TaxDialogBody(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -26,22 +28,11 @@ function TaxDialogBody(): JSX.Element {
 
   const isValidated = Utils.daysBetweenDates(fromDate, toDate) > 0;
 
-  const datePickerClassName = isValidated ? "" : "invalid";
-  const errorLabel = isValidated ?
-    <></> :
-    (
-      <div className="error-label">
-        <FontAwesomeIcon icon={faCircleExclamation} />
-        Intervallo selezionato non corretto
-      </div>
-    );
-
   function calculate(): void {
     async function fetchDataAsync(): Promise<void> {
       try {
         const response = await Api.fetchCityTaxAsync(fromDate, toDate);
         setCityTaxData(response.data);
-        setDialogState("done");
       } catch (error) {
         dispatch(ConnectionErrorSlice.show());
         setDialogState("fill");
@@ -59,29 +50,11 @@ function TaxDialogBody(): JSX.Element {
   case "fill":
     content = (
       <>
-        {errorLabel}
+        <ErrorLabel show={!isValidated} text="Intervallo selezionato non corretto" />
         <div className="row">
-          <div>
-            <span className="label">Dal:</span>
-            <DatePicker
-              className={datePickerClassName}
-              locale="it"
-              dateFormat="dd/MM/yyyy"
-              selected={new Date(fromDate)}
-              onChange={(date: Date) => { setFromDate(Utils.dateToString(date)); }}
-            />
-          </div>
-          <div>
-            <span className="label">Al:</span>
-            <DatePicker
-              className={datePickerClassName}
-              locale="it"
-              dateFormat="dd/MM/yyyy"
-              selected={new Date(toDate)}
-              onChange={(date: Date) => { setToDate(Utils.dateToString(date)); }}
-            />
-          </div>
-          <div className="button" onClick={calculate}>Calcola</div>
+          <LabeledDateInput id="from" label="Dal" isValid={isValidated} value={fromDate} onChange={setFromDate} />
+          <LabeledDateInput id="to" label="Al" isValid={isValidated} value={toDate} onChange={setToDate} />
+          <ButtonInput onClick={calculate}>Calcola</ButtonInput>
         </div>
       </>
     );
@@ -89,32 +62,20 @@ function TaxDialogBody(): JSX.Element {
 
   case "loading":
     content = (
-      <div className="row">
-        <div className="message">Calcolo...</div>
-      </div>
+      <LoadingDataWrapper data={cityTaxData}>
+        {(data) => (
+          <div className="dialog-table">
+            <DescriptionRow name="Standard" value={data.standard.toString()} />
+            <DescriptionRow name="Ragazzi sotto 14 anni" value={data.children.toString()} />
+            <DescriptionRow name="Permanenze oltre 10 giorni" value={data.over10Days.toString()} />
+            <div className="row centered">
+              <ButtonInput onClick={context.fadeOutDialog}>OK</ButtonInput>
+            </div>
+          </div>
+        )}
+      </LoadingDataWrapper>
     );
     break;
-
-  case "done":
-    content = (
-      <div className="dialog-table">
-        <div className="row">
-          <div>Standard:</div>
-          <div><b>{cityTaxData?.standard}</b></div>
-        </div>
-        <div className="row">
-          <div>Ragazzi sotto 14 anni:</div>
-          <div><b>{cityTaxData?.children}</b></div>
-        </div>
-        <div className="row">
-          <div>Permanenze oltre 10 giorni:</div>
-          <div><b>{cityTaxData?.over10Days}</b></div>
-        </div>
-        <div className="row centered">
-          <div className="button" onClick={context.fadeOutDialog}>OK</div>
-        </div>
-      </div>
-    );
   }
 
   return content;
