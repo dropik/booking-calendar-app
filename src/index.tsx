e2f718b32f5e881b4393d8e034a35f215c87cb33
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-04-18 04:50:13 +0200

    Updated react

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
@@ -9,11 +9,11 @@ import { store } from "./redux/store";
 
 import App from "./App";
 
-render(
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <Provider store={store}>
     <LocalizationProvider dateAdapter={AdapterDateFns} locale={itLocale}>
       <App />
     </LocalizationProvider>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
