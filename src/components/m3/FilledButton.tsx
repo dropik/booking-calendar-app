a75390873e2a1a6722269dc5bbfe0b6def8c0a11
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-04-19 10:15:41 +0200

    Implemented FilledButton

--- src/components/m3/FilledButton.tsx
+++ src/components/m3/FilledButton.tsx
@@ -4,19 +4,40 @@ import Typography from "@mui/material/Typography";
 import { alpha, styled } from "@mui/material/styles";
 
 const CustomizedButton = styled(Button)<ButtonProps>(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-  boxShadow: "none",
   color: theme.palette.onPrimary.main,
+  boxShadow: theme.shadows[0],
   textTransform: "none",
-  "&.Mui-disabled": {
+  height: "2.5rem",
+  borderRadius: "1.25rem",
+  paddingLeft: "1.5rem",
+  paddingRight: "1.5rem",
+  ":disabled": {
     backgroundColor: alpha(theme.palette.onSurface.main, 0.12),
     color: alpha(theme.palette.onSurface.main, 0.38)
+  },
+  ":hover": {
+    boxShadow: theme.shadows[1],
+    backgroundColor: theme.palette.primary.main
+  },
+  ":focus-visible": {
+    boxShadow: theme.shadows[0],
+    "& .MuiTouchRipple-root": {
+      backgroundColor: theme.palette.onPrimary.main,
+      opacity: 0.12
+    }
+  },
+  ":active": {
+    boxShadow: theme.shadows[0],
+    "& .MuiTouchRipple-root": {
+      backgroundColor: theme.palette.onPrimary.main,
+      opacity: 0.08
+    }
   }
 }));
 
 export default function FilledButton(props: ButtonProps): JSX.Element {
   return (
-    <CustomizedButton {...props} variant="contained">
+    <CustomizedButton {...props} variant="contained" disableTouchRipple disableFocusRipple>
       <Typography variant="labelLarge">
         {props.children}
       </Typography>
