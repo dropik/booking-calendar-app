31df997c5d13c200a98d8d4547fd6f5d6c7e6dc8
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-04-19 08:49:26 +0200

    Configured enabled and disabled states of FilledButton

--- src/components/m3/FilledButton.tsx
+++ src/components/m3/FilledButton.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Button, { ButtonProps } from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
+import { alpha, styled } from "@mui/material/styles";
 
 const CustomizedButton = styled(Button)<ButtonProps>(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
   boxShadow: "none",
   color: theme.palette.onPrimary.main,
+  textTransform: "none",
+  "&.Mui-disabled": {
+    backgroundColor: alpha(theme.palette.onSurface.main, 0.12),
+    color: alpha(theme.palette.onSurface.main, 0.38)
+  }
 }));
 
 export default function FilledButton(props: ButtonProps): JSX.Element {
