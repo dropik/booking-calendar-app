36d071e160c023c6c8f64e2576ca4cb5c0d327f5
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-04-22 15:18:31 +0200

    Moved sidemenu to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import TodayIcon from "@mui/icons-material/Today";
+import MenuIcon from "@mui/icons-material/Menu";
 import Box from "@mui/material/Box";
+
+import { useAppDispatch } from "../redux/hooks";
+import * as SidemenuSlice from "../redux/sidemenuSlice";
 
 import M3AppBar from "./m3/M3AppBar";
 import M3TextButton from "./m3/M3TextButton";
-import SidemenuButton from "./header/SidemenuButton";
 
 export default function Header(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  function showSidemenu() {
+    dispatch(SidemenuSlice.show());
+  }
+
   return (
     <M3AppBar>
-      <Box sx={{ display: "flex" }}>
-        <SidemenuButton />
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <M3TextButton
+          onClick={showSidemenu}
+          iconOnly
+          sx={{ color: (theme) => theme.palette.onSurface.main, mr: "1rem" }}
+        >
+          <MenuIcon />
+        </M3TextButton>
         <Typography variant="titleLarge">Booking Calendar</Typography>
       </Box>
       <M3TextButton
-        sx={{
-          color: (theme) => theme.palette.onSurfaceVariant.main
-        }}
+        sx={{ color: (theme) => theme.palette.onSurfaceVariant.main }}
         iconOnly
       >
         <TodayIcon />
