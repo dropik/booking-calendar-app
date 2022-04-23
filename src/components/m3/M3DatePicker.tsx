fab5d7a72174b05616a7769645b8afbff3597a77
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-04-23 23:30:42 +0200

    Using theme object instead of inline theme

--- src/components/m3/M3DatePicker.tsx
+++ src/components/m3/M3DatePicker.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { alpha } from "@mui/material/styles";
+import { alpha, useTheme } from "@mui/material/styles";
 import { DatePicker, DatePickerProps } from "@mui/x-date-pickers/DatePicker";
 
 export default function M3DatePicker<TDate>({ ...props }: DatePickerProps<TDate>): JSX.Element {
+  const theme = useTheme();
+
   return (
     <DatePicker
       {...props}
       PaperProps={{
         sx: {
-          backgroundColor: (theme) => theme.palette.surface.main,
-          boxShadow: (theme) => theme.shadows[2],
-          color: (theme) => theme.palette.onSurface.main,
+          backgroundColor: theme.palette.surface.main,
+          boxShadow: theme.shadows[2],
+          color: theme.palette.onSurface.main,
           borderRadius: "0.25rem",
           "& .MuiDialogActions-root": {
             position: "absolute",
@@ -20,17 +22,17 @@ export default function M3DatePicker<TDate>({ ...props }: DatePickerProps<TDate>
             left: 0,
             right: 0,
             zIndex: 1,
-            backgroundColor: (theme) => theme.palette.surfaceTint.main,
-            opacity: (theme) => theme.opacities.surface2
+            backgroundColor: theme.palette.surfaceTint.main,
+            opacity: theme.opacities.surface2
           },
           "& .MuiSvgIcon-root": {
-            color: (theme) => theme.palette.onSurfaceVariant.main
+            color: theme.palette.onSurfaceVariant.main
           },
           "& div[role=presentation]": {
             overflow: "visible"
           },
           "& .MuiCalendarPicker-viewTransitionContainer": {
-            borderTop: (theme) => `0.0625rem solid ${theme.palette.surfaceVariant.main}`
+            borderTop: `0.0625rem solid ${theme.palette.surfaceVariant.main}`
           },
           "& > div > div": {
             maxHeight: "22.4375rem",
@@ -39,17 +41,17 @@ export default function M3DatePicker<TDate>({ ...props }: DatePickerProps<TDate>
             }
           },
           "& .MuiButtonBase-root,button": {
-            color: (theme) => theme.palette.onSurface.main,
-            lineHeight: (theme) => theme.typography.labelLarge,
-            fontSize: (theme) => theme.typography.labelLarge,
-            fontWeight: (theme) => theme.typography.labelLarge,
-            letterSpacing: (theme) => theme.typography.labelLarge,
+            color: theme.palette.onSurface.main,
+            lineHeight: theme.typography.labelLarge,
+            fontSize: theme.typography.labelLarge,
+            fontWeight: theme.typography.labelLarge,
+            letterSpacing: theme.typography.labelLarge,
             "&:hover": {
-              backgroundColor: (theme) => alpha(theme.palette.onSurface.main, theme.opacities.hover)
+              backgroundColor: alpha(theme.palette.onSurface.main, theme.opacities.hover)
             },
             "&.Mui-selected": {
-              backgroundColor: (theme) => theme.palette.primary.main,
-              color: (theme) => theme.palette.onPrimary.main,
+              backgroundColor: theme.palette.primary.main,
+              color: theme.palette.onPrimary.main,
               "::before": {
                 content: "' '",
                 position: "absolute",
@@ -59,30 +61,30 @@ export default function M3DatePicker<TDate>({ ...props }: DatePickerProps<TDate>
                 left: 0,
                 right: 0,
                 bottom: 0,
-                backgroundColor: (theme) => theme.palette.onPrimary.main,
+                backgroundColor: theme.palette.onPrimary.main,
                 opacity: 0
               },
               "&:hover": {
-                boxShadow: (theme) => theme.shadows[1],
-                backgroundColor: (theme) => theme.palette.primary.main,
+                boxShadow: theme.shadows[1],
+                backgroundColor: theme.palette.primary.main,
                 "::before": {
-                  opacity: (theme) => theme.opacities.hover
+                  opacity: theme.opacities.hover
                 }
               },
               "&:focus": {
-                backgroundColor: (theme) => theme.palette.primary.main,
+                backgroundColor: theme.palette.primary.main,
               },
               "&:focus-visible": {
-                boxShadow: (theme) => theme.shadows[0],
-                backgroundColor: (theme) => theme.palette.primary.main,
+                boxShadow: theme.shadows[0],
+                backgroundColor: theme.palette.primary.main,
                 "::before": {
-                  opacity: (theme) => theme.opacities.focus
+                  opacity: theme.opacities.focus
                 }
               },
               "&:active": {
-                backgroundColor: (theme) => theme.palette.primary.main,
+                backgroundColor: theme.palette.primary.main,
                 "::before": {
-                  opacity: (theme) => theme.opacities.hover
+                  opacity: theme.opacities.hover
                 }
               }
             }
