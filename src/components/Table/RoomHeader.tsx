08ffb7ec81d291ec5fb732698bfbd3af5abfa57a
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-06-27 16:44:01 +0200

    Extracted row header into a component

---
+++ src/components/Table/RoomHeader.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { useTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
+import { RoomData } from "../../redux/hotelSlice";
+
+type Props = {
+  room: RoomData
+}
+
+export default function RowHeader({ room }: Props): JSX.Element {
+  const theme = useTheme();
+  const significantRoomType = room.type.replace("Camera ", "").replace("camera ", "");
+
+  return (
+    <Box sx={{
+      position: "absolute",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      left: 0,
+      top: 0,
+      bottom: 0,
+      width: "5.5rem",
+      pr: "1rem",
+      pl: "1rem",
+      borderRight: `1px solid ${theme.palette.outline.light}`
+    }}>
+      <Typography variant="labelLarge">{room.number}</Typography>
+      <Typography
+        sx={{
+          textAlign: "center",
+          overflowWrap: "anywhere"
+        }}
+        variant="bodySmall"
+      >
+        {significantRoomType}
+      </Typography>
+    </Box>
+  );
+}
--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -13,6 +13,7 @@ import M3IconButton from "../m3/M3IconButton";
 import DrawerAdjacent from "../m3/DrawerAdjacent";
 import FetchTiles from "../TableContainer/FetchTiles";
 import RowBody from "./RowBody";
+import RowHeader from "./RoomHeader";
 
 export default function Table(): JSX.Element {
   const theme = useTheme();
@@ -65,37 +66,12 @@ export default function Table(): JSX.Element {
                 }}>
                   {
                     floor.rooms.map((room, index) => {
-                      const significantRoomType = room.type.replace("Camera ", "").replace("camera ", "");
 
                       return (
                         <Box key={room.number} sx={{
                           position: "relative"
                         }}>
-                          <Box sx={{
-                            position: "absolute",
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            left: 0,
-                            top: 0,
-                            bottom: 0,
-                            width: "5.5rem",
-                            pr: "1rem",
-                            pl: "1rem",
-                            borderRight: `1px solid ${theme.palette.outline.light}`
-                          }}>
-                            <Typography variant="labelLarge">{room.number}</Typography>
-                            <Typography
-                              sx={{
-                                textAlign: "center",
-                                overflowWrap: "anywhere"
-                              }}
-                              variant="bodySmall"
-                            >
-                              {significantRoomType}
-                            </Typography>
-                          </Box>
+                          <RowHeader room={room} />
                           <RowBody isLast={index === floor.rooms.length - 1} roomNumber={room.number} />
                         </Box>
                       );
