31de5b82193d1fd5fec2956ebff5a8d7d3064c59
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-06-27 23:06:28 +0200

    Extracted tile container into a component

---
+++ src/components/Table/Floors/Rooms/RoomBody/DataRow/Tile/TileContainer.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode, useContext } from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+
+import { TileColor } from "../../../../../../../redux/tilesSlice";
+import { TileContext } from ".";
+
+type Props = {
+  children: ReactNode
+};
+
+export default function TileContainer({ children }: Props): JSX.Element {
+  const { data, cropLeft, cropRight } = useContext(TileContext);
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "3rem",
+        p: "1rem",
+        borderRadius: "0.75rem",
+        ...(cropLeft && {
+          borderTopLeftRadius: 0,
+          borderBottomLeftRadius: 0,
+        }),
+        ...(cropRight && {
+          borderTopRightRadius: 0,
+          borderBottomRightRadius: 0,
+        }),
+        backgroundColor: theme.palette[`${data.color}Container`].light,
+        color: theme.palette[`on${data.color[0].toUpperCase()}${data.color.substring(1)}Container` as `on${Capitalize<TileColor>}Container`].light,
+        border: `1px solid ${theme.palette.outline.light}`,
+        ...(cropRight && {
+          borderRight: 0
+        }),
+        ...(cropLeft && {
+          borderLeft: 0
+        })
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
--- src/components/Table/Floors/Rooms/RoomBody/DataRow/Tile/index.tsx
+++ src/components/Table/Floors/Rooms/RoomBody/DataRow/Tile/index.tsx
@@ -1,12 +1,12 @@
 import React, { createContext } from "react";
-import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 
-import { TileColor, TileData } from "../../../../../../../redux/tilesSlice";
+import { TileData } from "../../../../../../../redux/tilesSlice";
 import Title from "./Title";
 import Body from "./Body";
 import TileSize from "./TileSize";
 import TileAlert from "./TileAlert";
+import TileContainer from "./TileContainer";
 
 type Props = {
   data: TileData
@@ -35,59 +35,27 @@ export const TileContext = createContext<TileContextType>({
 });
 
 export default function Tile({ data }: Props): JSX.Element {
-  const theme = useTheme();
-
   return (
     <TileContext.Provider value={{ data: data, cropLeft: false, cropRight: false }}>
       <TileSize>
-        <TileContext.Consumer>
-          {(value) => (
-            <TileAlert>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  height: "3rem",
-                  p: "1rem",
-                  borderRadius: "0.75rem",
-                  ...(value.cropLeft && {
-                    borderTopLeftRadius: 0,
-                    borderBottomLeftRadius: 0,
-                  }),
-                  ...(value.cropRight && {
-                    borderTopRightRadius: 0,
-                    borderBottomRightRadius: 0,
-                  }),
-                  backgroundColor: theme.palette[`${data.color}Container`].light,
-                  color: theme.palette[`on${data.color[0].toUpperCase()}${data.color.substring(1)}Container` as `on${Capitalize<TileColor>}Container`].light,
-                  border: `1px solid ${theme.palette.outline.light}`,
-                  ...(value.cropRight && {
-                    borderRight: 0
-                  }),
-                  ...(value.cropLeft && {
-                    borderLeft: 0
-                  })
-                }}
-              >
-                <Box sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "start",
-                  justifyContent: "center",
-                  overflow: "hidden",
-                  "& > span": {
-                    width: "100%",
-                    whiteSpace: "nowrap"
-                  }
-                }}>
-                  <Title />
-                  <Body />
-                </Box>
-              </Box>
-            </TileAlert>
-          )}
-        </TileContext.Consumer>
+        <TileAlert>
+          <TileContainer>
+            <Box sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "start",
+              justifyContent: "center",
+              overflow: "hidden",
+              "& > span": {
+                width: "100%",
+                whiteSpace: "nowrap"
+              }
+            }}>
+              <Title />
+              <Body />
+            </Box>
+          </TileContainer>
+        </TileAlert>
       </TileSize>
     </TileContext.Provider>
   );
