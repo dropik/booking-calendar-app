113d614905de44dacdfed403d3a496de84c3372d
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-06-27 22:44:50 +0200

    Extracted tile size logic into a component

---
+++ src/components/Table/Floors/Rooms/RoomBody/DataRow/Tile/TileSize.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode, useContext } from "react";
+import Grid from "@mui/material/Grid";
+
+import * as Utils from "../../../../../../../utils";
+import { useAppSelector, useLeftmostDate } from "../../../../../../../redux/hooks";
+import { TileContext } from ".";
+
+type Props = {
+  children: ReactNode
+}
+
+export default function TileSize({ children }: Props): JSX.Element {
+  const data = useContext(TileContext).data;
+  const leftmostDate = useLeftmostDate();
+  const rightmostDate = useAppSelector((state) => Utils.getDateShift(state.table.leftmostDate, state.table.columns - 1));
+  const leftmostToArrival = Utils.daysBetweenDates(leftmostDate, data.from);
+  const arrivalToRightmost = Utils.daysBetweenDates(data.from, rightmostDate);
+
+  let cropLeft = false;
+  let cropRight = false;
+
+  let size = data.nights;
+  if (leftmostToArrival < 0) {
+    size -= -leftmostToArrival - 0.5;
+    cropLeft = true;
+  }
+  if (arrivalToRightmost < data.nights) {
+    size -= data.nights - arrivalToRightmost - 0.5;
+    cropRight = true;
+  }
+
+  return (
+    <Grid item xs={size} sx={{
+      ...(!cropRight && {
+        paddingRight: "1.5px"
+      }),
+      ...(!cropLeft && {
+        paddingLeft: "1.5px"
+      }),
+      pt: "1px",
+      pb: "1px"
+    }}>
+      <TileContext.Provider value={{ data: data, cropLeft: cropLeft, cropRight: cropRight }}>
+        {children}
+      </TileContext.Provider>
+    </Grid>
+  );
+}
--- src/components/Table/Floors/Rooms/RoomBody/DataRow/Tile/index.tsx
+++ src/components/Table/Floors/Rooms/RoomBody/DataRow/Tile/index.tsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { createContext } from "react";
 import { useTheme } from "@mui/material/styles";
-import Grid from "@mui/material/Grid";
 import Badge from "@mui/material/Badge";
 import Box from "@mui/material/Box";
 
-import * as Utils from "../../../../../../../utils";
 import { TileColor, TileData } from "../../../../../../../redux/tilesSlice";
-import { useAppSelector, useLeftmostDate } from "../../../../../../../redux/hooks";
+import { useAppSelector } from "../../../../../../../redux/hooks";
 import Title from "./Title";
 import Body from "./Body";
+import TileSize from "./TileSize";
 
 type Props = {
   data: TileData
 }
 
+type TileContextType = {
+  data: TileData,
+  cropLeft: boolean,
+  cropRight: boolean
+}
+
+export const TileContext = createContext<TileContextType>({
+  data: {
+    id: "",
+    bookingId: "",
+    name: "",
+    from: "",
+    nights: 0,
+    roomType: "",
+    entity: "",
+    persons: 0,
+    color: "booking1"
+  },
+  cropLeft: false,
+  cropRight: false
+});
+
 export default function Tile({ data }: Props): JSX.Element {
   const theme = useTheme();
-  const leftmostDate = useLeftmostDate();
-  const rightmostDate = useAppSelector((state) => Utils.getDateShift(state.table.leftmostDate, state.table.columns - 1));
-  const arrivalToRightmost = Utils.daysBetweenDates(data.from, rightmostDate);
-  const leftmostToArrival = Utils.daysBetweenDates(leftmostDate, data.from);
   const assignedRoomType = useAppSelector((state) => {
     for (const floor of state.hotel.data.floors) {
       for (const room of floor.rooms) {
@@ -31,7 +48,7 @@ export default function Tile({ data }: Props): JSX.Element {
   });
   const personsInAssignedRoomType = useAppSelector((state) => assignedRoomType ? state.roomTypes.data[assignedRoomType] : undefined);
 
-  let badgeColor = undefined;
+  let badgeColor = "";
   if (personsInAssignedRoomType) {
     if (!personsInAssignedRoomType.includes(data.persons)) {
       badgeColor = theme.palette.error.light;
@@ -40,96 +57,80 @@ export default function Tile({ data }: Props): JSX.Element {
     }
   }
 
-  let cropLeft = false;
-  let cropRight = false;
-
-  let size = data.nights;
-  if (leftmostToArrival < 0) {
-    size -= -leftmostToArrival - 0.5;
-    cropLeft = true;
-  }
-  if (arrivalToRightmost < data.nights) {
-    size -= data.nights - arrivalToRightmost - 0.5;
-    cropRight = true;
-  }
-
   return (
-    <Grid item xs={size} sx={{
-      ...(!cropRight && {
-        paddingRight: "1.5px"
-      }),
-      ...(!cropLeft && {
-        paddingLeft: "1.5px"
-      }),
-      pt: "1px",
-      pb: "1px"
-    }}>
-      <Badge
-        anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
-          ...(cropRight && {
-            horizontal: "left"
-          })
-        }}
-        badgeContent=" "
-        variant="dot"
-        sx={{
-          display: "block",
-          "& .MuiBadge-badge": {
-            backgroundColor: badgeColor,
-            top: "0.75rem",
-            right: "0.75rem",
-            ...(cropRight && {
-              right: "auto",
-              left: "0.75rem"
-            })
-          }
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            height: "3rem",
-            p: "1rem",
-            borderRadius: "0.75rem",
-            ...(cropLeft && {
-              borderTopLeftRadius: 0,
-              borderBottomLeftRadius: 0,
-            }),
-            ...(cropRight && {
-              borderTopRightRadius: 0,
-              borderBottomRightRadius: 0,
-            }),
-            backgroundColor: theme.palette[`${data.color}Container`].light,
-            color: theme.palette[`on${data.color[0].toUpperCase()}${data.color.substring(1)}Container` as `on${Capitalize<TileColor>}Container`].light,
-            border: `1px solid ${theme.palette.outline.light}`,
-            ...(cropRight && {
-              borderRight: 0
-            }),
-            ...(cropLeft && {
-              borderLeft: 0
-            })
-          }}
-        >
-          <Box sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-            justifyContent: "center",
-            overflow: "hidden",
-            "& > span": {
-              width: "100%",
-              whiteSpace: "nowrap"
-            }
-          }}>
-            <Title data={data} />
-            <Body data={data} />
-          </Box>
-        </Box>
-      </Badge>
-    </Grid>
+    <TileContext.Provider value={{ data: data, cropLeft: false, cropRight: false }}>
+      <TileSize>
+        <TileContext.Consumer>
+          {(value) => (
+            <Badge
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+                ...(value.cropRight && {
+                  horizontal: "left"
+                })
+              }}
+              badgeContent=" "
+              variant="dot"
+              sx={{
+                display: "block",
+                "& .MuiBadge-badge": {
+                  backgroundColor: badgeColor,
+                  top: "0.75rem",
+                  right: "0.75rem",
+                  ...(value.cropRight && {
+                    right: "auto",
+                    left: "0.75rem"
+                  })
+                }
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  height: "3rem",
+                  p: "1rem",
+                  borderRadius: "0.75rem",
+                  ...(value.cropLeft && {
+                    borderTopLeftRadius: 0,
+                    borderBottomLeftRadius: 0,
+                  }),
+                  ...(value.cropRight && {
+                    borderTopRightRadius: 0,
+                    borderBottomRightRadius: 0,
+                  }),
+                  backgroundColor: theme.palette[`${data.color}Container`].light,
+                  color: theme.palette[`on${data.color[0].toUpperCase()}${data.color.substring(1)}Container` as `on${Capitalize<TileColor>}Container`].light,
+                  border: `1px solid ${theme.palette.outline.light}`,
+                  ...(value.cropRight && {
+                    borderRight: 0
+                  }),
+                  ...(value.cropLeft && {
+                    borderLeft: 0
+                  })
+                }}
+              >
+                <Box sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "start",
+                  justifyContent: "center",
+                  overflow: "hidden",
+                  "& > span": {
+                    width: "100%",
+                    whiteSpace: "nowrap"
+                  }
+                }}>
+                  <Title data={data} />
+                  <Body data={data} />
+                </Box>
+              </Box>
+            </Badge>
+          )}
+        </TileContext.Consumer>
+      </TileSize>
+    </TileContext.Provider>
   );
 }
