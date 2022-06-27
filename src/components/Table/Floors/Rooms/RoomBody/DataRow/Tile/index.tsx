8d665405978fc46fa22cb8caf02aafdd58baf8a0
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-06-27 22:57:08 +0200

    Extracted tile alert into a component

--- src/components/Table/Floors/Rooms/RoomBody/DataRow/Tile/index.tsx
+++ src/components/Table/Floors/Rooms/RoomBody/DataRow/Tile/index.tsx
@@ -1,13 +1,12 @@
 import React, { createContext } from "react";
 import { useTheme } from "@mui/material/styles";
-import Badge from "@mui/material/Badge";
 import Box from "@mui/material/Box";
 
 import { TileColor, TileData } from "../../../../../../../redux/tilesSlice";
-import { useAppSelector } from "../../../../../../../redux/hooks";
 import Title from "./Title";
 import Body from "./Body";
 import TileSize from "./TileSize";
+import TileAlert from "./TileAlert";
 
 type Props = {
   data: TileData
@@ -37,54 +36,13 @@ export const TileContext = createContext<TileContextType>({
 
 export default function Tile({ data }: Props): JSX.Element {
   const theme = useTheme();
-  const assignedRoomType = useAppSelector((state) => {
-    for (const floor of state.hotel.data.floors) {
-      for (const room of floor.rooms) {
-        if (room.number === data.roomNumber) {
-          return room.type;
-        }
-      }
-    }
-  });
-  const personsInAssignedRoomType = useAppSelector((state) => assignedRoomType ? state.roomTypes.data[assignedRoomType] : undefined);
-
-  let badgeColor = "";
-  if (personsInAssignedRoomType) {
-    if (!personsInAssignedRoomType.includes(data.persons)) {
-      badgeColor = theme.palette.error.light;
-    } else if (assignedRoomType !== data.roomType) {
-      badgeColor = theme.palette.warning.dark;
-    }
-  }
 
   return (
     <TileContext.Provider value={{ data: data, cropLeft: false, cropRight: false }}>
       <TileSize>
         <TileContext.Consumer>
           {(value) => (
-            <Badge
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-                ...(value.cropRight && {
-                  horizontal: "left"
-                })
-              }}
-              badgeContent=" "
-              variant="dot"
-              sx={{
-                display: "block",
-                "& .MuiBadge-badge": {
-                  backgroundColor: badgeColor,
-                  top: "0.75rem",
-                  right: "0.75rem",
-                  ...(value.cropRight && {
-                    right: "auto",
-                    left: "0.75rem"
-                  })
-                }
-              }}
-            >
+            <TileAlert>
               <Box
                 sx={{
                   display: "flex",
@@ -127,7 +85,7 @@ export default function Tile({ data }: Props): JSX.Element {
                   <Body data={data} />
                 </Box>
               </Box>
-            </Badge>
+            </TileAlert>
           )}
         </TileContext.Consumer>
       </TileSize>
