228005dcf854eb533a9b68aae44a8324175d503e
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-08-07 18:49:53 +0200

    Implemented the look of dropzones

--- src/components/Table/Section/Floor/Room/Body/DataRow/index.tsx
+++ src/components/Table/Section/Floor/Room/Body/DataRow/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
 import * as Utils from "../../../../../../../utils";
 import { useAppSelector, useColumns, useDates, useLeftmostDate, useRightmostDate } from "../../../../../../../redux/hooks";
+import { TileData } from "../../../../../../../redux/tilesSlice";
 
 import FreeSpace from "./FreeSpace";
 import Tile from "./Tile";
+import { TileContext } from "./Tile/context";
+import TileSize from "./Tile/TileSize";
+import { SurfaceTint } from "../../../../../../m3/Tints";
 
 type Props = {
   roomNumber: number
@@ -43,7 +49,7 @@ function DateCellSwitch({ roomNumber, date }: DateCellSwitchProps): JSX.Element 
   if (assignedValue === "dropzone") {
     if (grabbedTile) {
       if ((date === grabbedTile.from) || (date === oneDayBefore)) {
-        return <DropZone />;
+        return <DropZone data={grabbedTile} />;
       }
     }
   } else if (assignedValue !== undefined) {
@@ -66,6 +72,54 @@ function DateCellSwitch({ roomNumber, date }: DateCellSwitchProps): JSX.Element 
   return null;
 }
 
-function DropZone(): JSX.Element {
-  return <></>;
+type DropZoneProps = {
+  data: TileData
+};
+
+function DropZone({ data }: DropZoneProps): JSX.Element {
+  const theme = useTheme();
+
+  return (
+    <TileContext.Provider value={{ data: data, cropLeft: false, cropRight: false}}>
+      <TileSize>
+        <TileContext.Consumer>
+          {
+            (value) => (
+              <Box
+                sx={{
+                  display: "flex",
+                  position: "relative",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  height: "3rem",
+                  p: "1rem",
+                  borderRadius: "0.75rem",
+                  ...(value.cropLeft && {
+                    borderTopLeftRadius: 0,
+                    borderBottomLeftRadius: 0,
+                  }),
+                  ...(value.cropRight && {
+                    borderTopRightRadius: 0,
+                    borderBottomRightRadius: 0,
+                  }),
+                  border: `1px dashed ${theme.palette.outline.light}`,
+                  ...(value.cropRight && {
+                    borderRight: 0
+                  }),
+                  ...(value.cropLeft && {
+                    borderLeft: 0
+                  })
+                }}
+              >
+                <SurfaceTint sx={{
+                  backgroundColor: theme.palette.primary.light,
+                  opacity: theme.opacities.surface1
+                }} />
+              </Box>
+            )
+          }
+        </TileContext.Consumer>
+      </TileSize>
+    </TileContext.Provider>
+  );
 }
