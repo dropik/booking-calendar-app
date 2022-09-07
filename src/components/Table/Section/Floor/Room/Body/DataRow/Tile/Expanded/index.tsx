641a626bc73ecc4b7026bc5fdeebcdfda73d42dc
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-07 09:08:53 +0200

    Implemented main part of extended tile card

--- src/components/Table/Section/Floor/Room/Body/DataRow/Tile/Expanded/index.tsx
+++ src/components/Table/Section/Floor/Room/Body/DataRow/Tile/Expanded/index.tsx
@@ -1,5 +1,8 @@
 import React, { useContext } from "react";
+import { useTheme } from "@mui/material/styles";
 import Popover from "@mui/material/Popover";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 import * as Utils from "../../../../../../../../../utils";
 import { TileContext } from "../context";
@@ -11,12 +14,19 @@ type ExpandedProps = {
 
 export default function Expanded({ anchorEl, onClose }: ExpandedProps): JSX.Element {
   const { data } = useContext(TileContext);
+  const theme = useTheme();
 
   const open = Boolean(anchorEl);
   const id = open ? "expanded-tile" : undefined;
   const anchorElWidthPx = anchorEl ? anchorEl.getBoundingClientRect().width : 0;
   const anchorElWidthRem = Utils.pxToRem(anchorElWidthPx);
   const anchorElWidthRemCaped = Math.max(anchorElWidthRem, 22.5);
+
+  const personsStr = `${data.persons} person${data.persons === 1 ? "a" : "e"}`;
+  const formattedFrom = (new Date(data.from)).toLocaleDateString();
+  const formattedTo = (new Date(Utils.getDateShift(data.from, data.nights))).toLocaleDateString();
+  const periodStr = `${formattedFrom} - ${formattedTo}`;
+  const formattedRoomType = `${data.entity[0].toLocaleUpperCase()}${data.entity.slice(1)}`;
 
   return (
     <Popover
@@ -35,11 +45,19 @@ export default function Expanded({ anchorEl, onClose }: ExpandedProps): JSX.Elem
       elevation={1}
       PaperProps={{
         sx: {
-          width: `${anchorElWidthRemCaped}rem`
+          width: `${anchorElWidthRemCaped}rem`,
+          borderRadius: "0.75rem"
         }
       }}
     >
-      Tile
+      <Stack spacing={0} sx={{ p: "1rem", backgroundColor: theme.palette[`${data.color}Container`].light }}>
+        <Typography variant="headlineMedium">{data.name}</Typography>
+        <Typography variant="titleMedium">{personsStr}</Typography>
+        <Stack sx={{ pt: "0.5rem" }}>
+          <Typography variant="bodySmall">{periodStr}</Typography>
+          <Typography variant="bodySmall">{formattedRoomType}</Typography>
+        </Stack>
+      </Stack>
     </Popover>
   );
 }
