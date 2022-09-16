a61830f51d7034d25eef704205eb4ddc64b63f31
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-16 23:39:05 +0200

    Preventing api call if input is not valid

--- src/components/Bookings/index.tsx
+++ src/components/Bookings/index.tsx
@@ -15,42 +15,49 @@ export default function Bookings(): JSX.Element {
   const [to, setTo] = useState(Utils.getDateShift(currentDate, 1));
   const [name, setName] = useState("");
   const [bookings, setBookings] = useState<BookingShortData[]>([]);
+  const [isFromValid, setIsFromValid] = useState(true);
+  const [isToValid, setIsToValid] = useState(true);
+
+  const isValid = isFromValid && isToValid;
 
   useEffect(() => {
     let subscribed = true;
 
     async function fetchData() {
-      const response = await fetchBookings(name, from, to);
-      if (subscribed) {
-        setBookings(response.data);
+      if (isValid) {
+        const response = await fetchBookings(name, from, to);
+        if (subscribed) {
+          setBookings(response.data);
+        }
       }
     }
 
     fetchData();
 
     return () => { subscribed = false; };
-  }, [name, from, to]);
+  }, [name, from, to, isValid]);
 
   return (
-    <Stack spacing={2} direction="row" sx={{
-      pl: "1rem",
-      pr: "1rem"
-    }}>
+    <Stack spacing={2} direction="row" sx={{ pl: "1rem", pr: "1rem" }}>
       <Stack spacing={0} sx={{ width: "25rem" }}>
-        <Stack spacing={1}>
-          <Stack spacing={1} direction="row" sx={{ pt: "0.5rem" }}>
+        <Stack spacing={1} sx={{ pt: "1rem", pb: "1rem" }}>
+          <Stack spacing={1} direction="row">
             <M3DatePicker
               value={new Date(from)}
               onChange={(date: Date | null) => {
                 if (date) {
                   setFrom(Utils.dateToString(date));
+                  setIsFromValid(false);
                 }
               }}
+              onAccept={() => setIsFromValid(true)}
+              onError={(reason) => setIsFromValid(reason === null)}
               renderInput={({ error, ...props }) => (
                 <TextField
                   {...props}
                   id="from"
                   label="Dal"
+                  onBlur={() => setIsFromValid(!error)}
                   error={error}
                   helperText={error ? "Periodo non valido" : null}
                 />
@@ -64,13 +71,17 @@ export default function Bookings(): JSX.Element {
               onChange={(date: Date | null) => {
                 if (date) {
                   setTo(Utils.dateToString(date));
+                  setIsToValid(false);
                 }
               }}
+              onAccept={() => setIsToValid(true)}
+              onError={(reason) => setIsToValid(reason === null)}
               renderInput={({ error, ...props }) => (
                 <TextField
                   {...props}
                   id="to"
                   label="Al"
+                  onBlur={() => setIsToValid(!error)}
                   error={error}
                   helperText={error ? "Periodo non valido" : null}
                 />
