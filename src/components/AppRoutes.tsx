fccad062424d6f95369b985960873b9fa8b79f5c
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-16 16:53:09 +0200

    Created bookings page base

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import DrawerAdjacent from "./m3/DrawerAdjacent";
 import Table from "./Table";
+import Bookings from "./Bookings";
 
 export default function AppRoutes(): JSX.Element {
   return (
     <DrawerAdjacent>
       <Routes>
         <Route path="/" element={<Table />} />
-        <Route path="/bookings" element={"Prenotazioni"} />
+        <Route path="/bookings" element={<Bookings />} />
         <Route path="/tools" element={"Strumenti"} />
         <Route path="/clients" element={"Clienti"} />
       </Routes>
