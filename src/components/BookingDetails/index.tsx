9fbe6f73c998aaa329ac7d64755a2fdcb5624530
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-18 23:59:27 +0200

    Created background for details

--- src/components/BookingDetails/index.tsx
+++ src/components/BookingDetails/index.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import { SurfaceTint } from "../m3/Tints";
+import { useTheme } from "@mui/material/styles";
 
 export default function BookingDetails(): JSX.Element {
+  const theme = useTheme();
+
   return (
-    <Box>Ivan Petrov</Box>
+    <Stack spacing={2} sx={{
+      position: "relative",
+      flexGrow: 1
+    }}>
+      <SurfaceTint sx={{
+        backgroundColor: theme.palette.primary.light,
+        opacity: theme.opacities.surface1
+      }} />
+    </Stack>
   );
 }
