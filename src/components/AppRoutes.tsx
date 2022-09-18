a06873554ffd936d2f23992f3483b36d0f99a867
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-18 20:42:38 +0200

    Created base for booking details

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import DrawerAdjacent from "./m3/DrawerAdjacent";
 import Table from "./Table";
 import Bookings from "./Bookings";
+import BookingDetails from "./BookingDetails";
 
 export default function AppRoutes(): JSX.Element {
   return (
@@ -11,7 +12,7 @@ export default function AppRoutes(): JSX.Element {
       <Routes>
         <Route path="/" element={<Table />} />
         <Route path="bookings" element={<Bookings />}>
-          <Route path=":bookingId" element={"Ivan Petrov"} />
+          <Route path=":bookingId" element={<BookingDetails />} />
         </Route>
         <Route path="tools" element={"Strumenti"} />
         <Route path="clients" element={"Clienti"} />
