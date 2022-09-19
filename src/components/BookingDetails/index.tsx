7c67f2bd393881bdd077ea1f7eb31165e6921555
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-19 19:28:21 +0200

    Implemented data fetch on detail open

--- src/components/BookingDetails/index.tsx
+++ src/components/BookingDetails/index.tsx
@@ -1,20 +1,47 @@
-import React from "react";
-import Stack from "@mui/material/Stack";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
+
+import { BookingData, fetchBookingById } from "../../api";
 
 export default function BookingDetails(): JSX.Element {
   const theme = useTheme();
+  const { bookingId } = useParams();
+  const [data, setData] = useState<BookingData | undefined>(undefined);
+
+  useEffect(() => {
+    let isSubscribed = true;
+
+    async function fetchData() {
+      if (bookingId) {
+        const response = await fetchBookingById(bookingId);
+
+        if (isSubscribed) {
+          setData(response.data);
+        }
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      isSubscribed = false;
+      setData(undefined);
+    };
+  }, [bookingId]);
 
   return (
     <Stack spacing={2} sx={{
       position: "relative",
+      height: "calc(100vh - 10.5rem)",
       flexGrow: 1,
       backgroundColor: theme.palette.surfaceVariant.light,
       color: theme.palette.onSurfaceVariant.light,
       borderBottomRightRadius: "0.75rem",
       borderBottomLeftRadius: "0.75rem"
     }}>
-      Ivan Petrov
+      {data?.name}
     </Stack>
   );
 }
