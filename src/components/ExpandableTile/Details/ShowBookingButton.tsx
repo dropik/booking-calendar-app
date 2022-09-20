6e1599bb01373ad901dd6b95b88e2804c4dbccf2
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-20 20:49:33 +0200

    Hiding go to booking button for in-content tiles

--- src/components/ExpandableTile/Details/ShowBookingButton.tsx
+++ src/components/ExpandableTile/Details/ShowBookingButton.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import Stack from "@mui/material/Stack";
 
+import ExpandableTileContext from "../context";
 import M3TextButton from "../../m3/M3TextButton";
 
-export default function ShowBookingButton(): JSX.Element {
+export default function ShowBookingButton(): JSX.Element | null {
+  const { variant } = useContext(ExpandableTileContext);
+
+  if (variant === "in-content") {
+    return null;
+  }
+
   return (
     <Stack direction="row" justifyContent="end">
       <M3TextButton>Mostra prenotazione</M3TextButton>
