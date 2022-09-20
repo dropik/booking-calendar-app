8a3a2734b5adc87ac6b5c3e172abfd29bb4fb8fa
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-20 19:41:54 +0200

    Disabled colorized header for in-content tiles

--- src/components/ExpandableTile/Header/Container.tsx
+++ src/components/ExpandableTile/Header/Container.tsx
@@ -4,6 +4,7 @@ import Stack from "@mui/material/Stack";
 
 import { TileColor } from "../../../redux/tilesSlice";
 import { TileContext } from "../../Tile/context";
+import ExpandableTileContext from "../context";
 
 type ContainerProps = {
   children: React.ReactNode
@@ -11,14 +12,15 @@ type ContainerProps = {
 
 const Container = forwardRef<HTMLDivElement, ContainerProps>(function Container({ children }, ref): JSX.Element {
   const { data } = useContext(TileContext);
+  const { variant } = useContext(ExpandableTileContext);
   const theme = useTheme();
 
   return (
     <Stack spacing={0} sx={{
       p: "1rem",
       borderRadius: "inherit",
-      backgroundColor: theme.palette[`${data.color}Container`].light,
-      color: theme.palette[`on${data.color[0].toUpperCase()}${data.color.substring(1)}Container` as `on${Capitalize<TileColor>}Container`].light
+      backgroundColor: variant === "popup" ? theme.palette[`${data.color}Container`].light : undefined,
+      color: variant === "popup" ? theme.palette[`on${data.color[0].toUpperCase()}${data.color.substring(1)}Container` as `on${Capitalize<TileColor>}Container`].light : undefined
     }} ref={ref}>
       {children}
     </Stack>
--- src/components/ExpandableTile/Tint.tsx
+++ src/components/ExpandableTile/Tint.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useTheme } from "@mui/material/styles";
 
+import ExpandableTileContext from "./context";
 import { SurfaceTint } from "../m3/Tints";
 
 export default function Tint(): JSX.Element {
+  const { variant } = useContext(ExpandableTileContext);
   const theme = useTheme();
 
   return (
     <SurfaceTint sx={{
       backgroundColor: theme.palette.primary.light,
-      opacity: theme.opacities.surface1
+      opacity: variant === "popup" ? theme.opacities.surface1 : theme.opacities.surface2
     }} />
   );
 }
