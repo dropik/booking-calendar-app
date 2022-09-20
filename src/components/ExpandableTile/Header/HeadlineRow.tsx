aff502e064bf1cf740acbe44820db9208259e78b
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-20 19:36:32 +0200

    Disabled more button for in-content tiles

--- src/components/ExpandableTile/Header/HeadlineRow.tsx
+++ src/components/ExpandableTile/Header/HeadlineRow.tsx
@@ -3,16 +3,18 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 
 import { TileContext } from "../../Tile/context";
+import ExpandableTileContext from "../context";
 
 import MoreButton from "./MoreButton";
 
 export default function HeadlineRow(): JSX.Element {
   const { data } = useContext(TileContext);
+  const { variant } = useContext(ExpandableTileContext);
 
   return (
     <Stack direction="row" justifyContent="space-between">
       <Typography variant="headlineMedium">{data.name}</Typography>
-      <MoreButton />
+      {variant === "popup" ? <MoreButton /> : null}
     </Stack>
   );
 }
