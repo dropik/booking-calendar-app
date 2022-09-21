3a7daf1bf36560b0372c3172cef34d712e0f49a3
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-21 19:12:00 +0200

    Implemented layout for card details

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useTheme } from "@mui/material/styles";
 import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -14,6 +13,7 @@ import { ClientData, fetchClients } from "../../api";
 
 import DrawerAdjacent from "../m3/DrawerAdjacent";
 import M3IconButton from "../m3/M3IconButton";
+import M3TextButton from "../m3/M3TextButton";
 
 export default function Clients(): JSX.Element {
   const [query, setQuery] = useState("");
@@ -45,7 +45,7 @@ export default function Clients(): JSX.Element {
 
   return (
     <DrawerAdjacent>
-      <Stack spacing={2} sx={{ pr: "1rem" }}>
+      <Stack spacing={2} sx={{ pr: "1rem", pb: "0.5rem" }}>
         <Stack
           spacing={0}
           direction="row"
@@ -76,27 +76,46 @@ export default function Clients(): JSX.Element {
           </Stack>
         </Stack>
         <Grid container spacing={0} direction="row" columns={4}>
-          {clients.map((client) => (
-            <Grid item key={client.id} xs={1} >
-              <Box sx={{
-                borderRadius: "0.75rem",
-                border: `1px solid ${theme.palette.outline.light}`,
-                mr: "0.5rem",
-                mb: "0.5rem"
-              }}>
-                <Stack sx={{ p: "1rem" }}>
-                  <Typography variant="headlineMedium">{`${client.name} ${client.surname}`}</Typography>
-                  <Typography variant="titleMedium">{(new Date(client.dateOfBirth).toLocaleDateString())}</Typography>
-                  <Stack sx={{ pt: "0.5rem" }} justifyContent="space-between" direction="row">
-                    <Typography variant="bodySmall">
-                      {`${client.placeOfBirth ? `${client.placeOfBirth} - ` : ""}${client.stateOfBirth}`}
-                    </Typography>
-                    <M3IconButton><ExpandMoreOutlined /></M3IconButton>
+          {clients.map((client) => {
+            const formattedFrom = (new Date(client.booking.from)).toLocaleDateString();
+            const formattedTo = (new Date(client.booking.to)).toLocaleDateString();
+            const periodStr = `${formattedFrom} - ${formattedTo}`;
+
+            return (
+              <Grid item key={client.id} xs={1} >
+                <Stack sx={{
+                  borderRadius: "0.75rem",
+                  border: `1px solid ${theme.palette.outline.light}`,
+                  mr: "0.5rem",
+                  mb: "0.5rem"
+                }}>
+                  <Stack sx={{ p: "1rem" }}>
+                    <Typography variant="headlineMedium">{`${client.name} ${client.surname}`}</Typography>
+                    <Typography variant="titleMedium">{(new Date(client.dateOfBirth).toLocaleDateString())}</Typography>
+                    <Stack sx={{ pt: "0.5rem" }} justifyContent="space-between" alignItems="center" direction="row">
+                      <Typography variant="bodySmall">
+                        {`${client.placeOfBirth ? `${client.placeOfBirth} - ` : ""}${client.stateOfBirth}`}
+                      </Typography>
+                      <M3IconButton><ExpandMoreOutlined /></M3IconButton>
+                    </Stack>
+                  </Stack>
+                  <Stack spacing={2} sx={{
+                    p: "1rem",
+                    borderTop: `1px solid ${theme.palette.outline.light}`
+                  }}>
+                    <Typography variant="titleLarge">Prenotazione</Typography>
+                    <Stack sx={{ pl: "1rem" }}>
+                      <Typography variant="titleMedium">{client.booking.name}</Typography>
+                      <Typography variant="bodySmall">{periodStr}</Typography>
+                    </Stack>
+                    <Stack alignItems="flex-end">
+                      <M3TextButton>Mostra prenotazione</M3TextButton>
+                    </Stack>
                   </Stack>
                 </Stack>
-              </Box>
-            </Grid>
-          ))}
+              </Grid>
+            );
+          })}
         </Grid>
       </Stack>
     </DrawerAdjacent>
