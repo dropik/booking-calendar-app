51c03f3beb4961fd611d12b5153a0d417790a594
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-21 11:43:03 +0200

    Going to bookings page on show booking button click

--- src/components/ExpandableTile/Details/ShowBookingButton.tsx
+++ src/components/ExpandableTile/Details/ShowBookingButton.tsx
@@ -1,11 +1,14 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import Stack from "@mui/material/Stack";
 
 import ExpandableTileContext from "../context";
+import { TileContext } from "../../Tile/context";
 import M3TextButton from "../../m3/M3TextButton";
 
 export default function ShowBookingButton(): JSX.Element | null {
   const { variant } = useContext(ExpandableTileContext);
+  const { data } = useContext(TileContext);
 
   if (variant === "in-content") {
     return null;
@@ -13,7 +16,9 @@ export default function ShowBookingButton(): JSX.Element | null {
 
   return (
     <Stack direction="row" justifyContent="end">
-      <M3TextButton>Mostra prenotazione</M3TextButton>
+      <Link to={`/bookings/${data.bookingId}`} style={{ textDecoration: "none" }}>
+        <M3TextButton>Mostra prenotazione</M3TextButton>
+      </Link>
     </Stack>
   );
 }
