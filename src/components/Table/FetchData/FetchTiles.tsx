1bf87aeff19211eb883e392b2ba46731b5ba9e7e
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-27 08:49:27 +0200

    Real-time tiles update

--- src/components/Table/FetchData/FetchTiles.tsx
+++ src/components/Table/FetchData/FetchTiles.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { useAppSelector } from "../../../redux/hooks";
 import { fetchAsync } from "../../../redux/tilesSlice";
@@ -7,6 +7,12 @@ import FetchDataBase from "./FetchDataBase";
 
 export default function FetchTiles(): JSX.Element {
   const args = useAppSelector((state) => state.table.lastFetchPeriod);
+  const [, setUpdateRequest] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => setUpdateRequest((r) => r + 1), 180000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <FetchDataBase fetchCallbackAsync={fetchAsync({ from: args.from, to: args.to })} />
