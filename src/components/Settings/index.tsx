01a14a2f63dcb7b65a2a1285eb7ef02adb8a4499
Daniil Ryzhkov <drop.sovet@gmail.com>
2022-09-28 17:18:10 +0200

    Better page layout

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,16 +1,17 @@
+import React from "react";
+import { useTheme } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import React from "react";
-import { useAppSelector, useFloors } from "../../redux/hooks";
-import DrawerAdjacent from "../m3/DrawerAdjacent";
-import M3IconButton from "../m3/M3IconButton";
-import { SurfaceTint } from "../m3/Tints";
-import { useTheme } from "@mui/material/styles";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+
+import { useAppSelector, useFloors } from "../../redux/hooks";
+import DrawerAdjacent from "../m3/DrawerAdjacent";
+import M3IconButton from "../m3/M3IconButton";
+import { SurfaceTint } from "../m3/Tints";
 import M3Fab from "../m3/M3Fab";
 
 export default function Settings(): JSX.Element {
@@ -46,37 +47,33 @@ export default function Settings(): JSX.Element {
                     </Stack>
                     <M3IconButton><DeleteOutlineOutlinedIcon /></M3IconButton>
                   </Stack>
-                  <Stack spacing={2}>
-                    <Stack spacing={1}>
-                      {roomIds.map((roomId) => {
-                        const room = floor.rooms[roomId];
-                        const roomType = `${room.type[0].toLocaleUpperCase()}${room.type.slice(1)}`;
+                  <Stack spacing={1}>
+                    {roomIds.map((roomId) => {
+                      const room = floor.rooms[roomId];
+                      const roomType = `${room.type[0].toLocaleUpperCase()}${room.type.slice(1)}`;
 
-                        return (
-                          <Paper key={roomId} elevation={1} sx={{
-                            position: "relative",
-                            p: "1rem",
-                            borderRadius: "0.75rem",
-                            overflow: "hidden"
-                          }}>
-                            <Stack direction="row" justifyContent="space-between" alignItems="center">
-                              <Stack sx={{ flexGrow: 1 }}>
-                                <Typography variant="headlineSmall">{`Camera ${room.number}`}</Typography>
-                                <Typography variant="labelLarge">{roomType}</Typography>
-                              </Stack>
-                              <Stack direction="row">
-                                <M3IconButton><EditOutlinedIcon /></M3IconButton>
-                                <M3IconButton><DeleteOutlineOutlinedIcon /></M3IconButton>
-                              </Stack>
+                      return (
+                        <Paper key={roomId} elevation={0} sx={{
+                          position: "relative",
+                          p: "1rem",
+                          borderRadius: "0.75rem",
+                          overflow: "hidden",
+                          backgroundColor: theme.palette.surfaceVariant.light,
+                          color: theme.palette.onSurfaceVariant.light
+                        }}>
+                          <Stack direction="row" justifyContent="space-between" alignItems="center">
+                            <Stack sx={{ flexGrow: 1 }}>
+                              <Typography variant="headlineSmall">{`Camera ${room.number}`}</Typography>
+                              <Typography variant="labelLarge">{roomType}</Typography>
                             </Stack>
-                            <SurfaceTint sx={{
-                              backgroundColor: theme.palette.primary.light,
-                              opacity: theme.opacities.surface1
-                            }} />
-                          </Paper>
-                        );
-                      })}
-                    </Stack>
+                            <Stack direction="row">
+                              <M3IconButton><EditOutlinedIcon /></M3IconButton>
+                              <M3IconButton><DeleteOutlineOutlinedIcon /></M3IconButton>
+                            </Stack>
+                          </Stack>
+                        </Paper>
+                      );
+                    })}
                     <M3IconButton><AddCircleOutlineOutlinedIcon /></M3IconButton>
                   </Stack>
                 </Stack>
@@ -96,6 +93,10 @@ export default function Settings(): JSX.Element {
           <AddOutlinedIcon />
           <Typography variant="labelLarge">Crea</Typography>
         </Stack>
+        <SurfaceTint sx={{
+          backgroundColor: theme.palette.primary.light,
+          opacity: theme.opacities.surface3
+        }} />
       </M3Fab>
     </DrawerAdjacent>
   );
