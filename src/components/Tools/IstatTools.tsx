eee0dcfed8bf83749d8eefd649006ab40e54f2ee
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2023-02-12 18:51:04 +0100

    Istat button can be toggled to open a dialog

--- src/components/Tools/IstatTools.tsx
+++ src/components/Tools/IstatTools.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import M3Chip from "../m3/M3Chip";
+import M3Dialog from "../m3/M3Dialog";
 
 export default function IstatTools(): JSX.Element {
+  const [selected, setSelected] = useState(false);
+
+  function open(): void {
+    setSelected(true);
+  }
+
+  function close(): void {
+    setSelected(false);
+  }
+
   return (
-    <M3Chip selected={false} onClick={() => void 0} label="ISTAT" />
+    <>
+      <M3Chip selected={selected} onClick={open} label="ISTAT" />
+      <M3Dialog open={selected} onClose={close} heightRem={10}>
+
+      </M3Dialog>
+    </>
   );
 }
