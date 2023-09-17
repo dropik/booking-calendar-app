868240a4e6f3779e29f5f0699e054e5aeb91604d
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2023-09-17 21:08:45 +0200

    Added top bar to tools page

--- src/components/Tools/Dashboard.tsx
+++ src/components/Tools/Dashboard.tsx
@@ -21,6 +21,8 @@ import { SurfaceTint } from "../m3/Tints";
 import M3Skeleton from "../m3/M3Skeleton";
 import M3Chip from "../m3/M3Chip";
 import { setTokens } from "../../redux/authSlice";
+import TopAppBar from "../TopAppBar";
+import UpperHeader from "../TopAppBar/UpperHeader";
 
 export default function Dashboard(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -65,145 +67,150 @@ export default function Dashboard(): JSX.Element {
   }, [dispatch, downloadDate, getPoliceRicevuta]);
 
   return (
-    <Stack spacing={1} sx={{ pr: "1rem" }}>
-      <Typography variant="displaySmall" sx={{ pt: "3rem", pl: "1rem" }}>Strumenti</Typography>
-      <Stack direction="row" spacing={1}>
-        <NavLink to="istat">
-          <M3Chip selected={false} onClick={() => void 0} label="ISTAT" />
-        </NavLink>
-      </Stack>
-      <Stack direction="row" spacing={2} sx={{ mt: "2rem !important" }}>
-        <Stack spacing={2} sx={{
-          position: "relative",
-          justifyContent: "space-between",
-          p: "1rem",
-          borderRadius: "0.75rem",
-          flexShrink: 1,
-          color: theme.palette.onSurfaceVariant.light
-        }}>
-          <Stack spacing={2}>
-            <Typography variant="headlineMedium">Esporta dati</Typography>
-            <M3DatePicker
-              value={new Date(downloadDate)}
-              onChange={(date: Date | null) => {
-                if (date) {
-                  setDownloadDate(Utils.dateToString(date));
-                }
-              }}
-              renderInput={(props) => <TextField {...props} />}
-            />
-          </Stack>
-          <a ref={anchorRef}></a>
-          <Stack spacing={1} direction="row" justifyContent="flex-end">
-            {isPoliceLoading ? <CircularProgress color="primary" /> : (
-              <>
-                <M3TextButton onClick={() => getPoliceRicevuta.query(downloadDate)}>Scarica ricevuta</M3TextButton>
-                <M3TextButton onClick={() => postPoliceRicevuta({ date: downloadDate })}>Polizia</M3TextButton>
-              </>
-            )}
-          </Stack>
-          <SurfaceTint sx={{
-            backgroundColor: theme.palette.primary.light,
-            opacity: theme.opacities.surface2,
-            m: "0 !important"
-          }} />
+    <>
+      <TopAppBar>
+        <UpperHeader />
+      </TopAppBar>
+      <Stack spacing={1} sx={{ pr: "1rem" }}>
+        <Typography variant="displaySmall" sx={{ pl: "1rem" }}>Strumenti</Typography>
+        <Stack direction="row" spacing={1}>
+          <NavLink to="istat">
+            <M3Chip selected={false} onClick={() => void 0} label="ISTAT" />
+          </NavLink>
         </Stack>
-        <Stack direction="row" sx={{
-          border: `1px solid ${theme.palette.outline.light}`,
-          borderRadius: "0.75rem"
-        }}>
+        <Stack direction="row" spacing={2} sx={{ mt: "2rem !important" }}>
           <Stack spacing={2} sx={{
             position: "relative",
             justifyContent: "space-between",
             p: "1rem",
             borderRadius: "0.75rem",
-            color: theme.palette.onSurfaceVariant.light,
+            flexShrink: 1,
+            color: theme.palette.onSurfaceVariant.light
           }}>
             <Stack spacing={2}>
-              <Typography variant="headlineMedium">Tassa di soggiorno</Typography>
-              <Stack direction={drawerOpened ? "column" : "row"} spacing={1}>
-                <M3DatePicker
-                  value={new Date(from)}
-                  onChange={(date: Date | null) => {
-                    if (date) {
-                      setIsFromValid(false);
-                      setFrom(Utils.dateToString(date));
-                    }
-                  }}
-                  onAccept={() => setIsFromValid(true)}
-                  onError={(reason) => setIsFromValid(reason === null)}
-                  shouldDisableDate={(date) => Utils.daysBetweenDates(to, Utils.dateToString(date)) > 0}
-                  renderInput={({ error, ...props }) => (
-                    <TextField
-                      {...props}
-                      label="Dal"
-                      error={error}
-                      helperText={error ? "Periodo non valido" : undefined}
-                      onBlur={() => setIsFromValid(!error)}
-                    />
-                  )}
-                />
-                <M3DatePicker
-                  value={new Date(to)}
-                  onChange={(date: Date | null) => {
-                    if (date) {
-                      setIsToValid(false);
-                      setTo(Utils.dateToString(date));
-                    }
-                  }}
-                  onAccept={() => setIsToValid(true)}
-                  onError={(reason) => setIsToValid(reason === null)}
-                  shouldDisableDate={(date) => Utils.daysBetweenDates(Utils.dateToString(date), from) > 0}
-                  renderInput={({ error, ...props }) => (
-                    <TextField
-                      {...props}
-                      label="Al"
-                      error={error}
-                      helperText={error ? "Periodo non valido" : undefined}
-                      onBlur={() => setIsToValid(!error)}
-                    />
-                  )}
-                />
-              </Stack>
-            </Stack>
-            <Stack direction="row" justifyContent="flex-end">
-              {cityTaxResult.isFetching ?
-                <CircularProgress color="primary" /> :
-                <M3TextButton onClick={() => {
-                  if (isCityTaxValid) {
-                    getCityTaxTrigger({ from, to });
+              <Typography variant="headlineMedium">Esporta dati</Typography>
+              <M3DatePicker
+                value={new Date(downloadDate)}
+                onChange={(date: Date | null) => {
+                  if (date) {
+                    setDownloadDate(Utils.dateToString(date));
                   }
-                }}>Calcola</M3TextButton>
-              }
+                }}
+                renderInput={(props) => <TextField {...props} />}
+              />
+            </Stack>
+            <a ref={anchorRef}></a>
+            <Stack spacing={1} direction="row" justifyContent="flex-end">
+              {isPoliceLoading ? <CircularProgress color="primary" /> : (
+                <>
+                  <M3TextButton onClick={() => getPoliceRicevuta.query(downloadDate)}>Scarica ricevuta</M3TextButton>
+                  <M3TextButton onClick={() => postPoliceRicevuta({ date: downloadDate })}>Polizia</M3TextButton>
+                </>
+              )}
             </Stack>
             <SurfaceTint sx={{
               backgroundColor: theme.palette.primary.light,
               opacity: theme.opacities.surface2,
               m: "0 !important"
-            }}
-            />
+            }} />
           </Stack>
-          <Box>
-            <Collapse in={openDetails} orientation="horizontal">
-              <Stack spacing={2} sx={{ p: "1rem" }}>
-                <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ pt: "1rem", pb: "1rem" }}>
-                  <Typography variant="titleMedium" sx={{ whiteSpace: "nowrap" }}>Regolari</Typography>
-                  <Typography variant="bodyMedium" sx={{ whiteSpace: "nowrap" }}>{!cityTaxResult.isFetching ? `${cityTaxResult.data?.standard ?? 0} presenze` : <M3Skeleton width="4rem" />}</Typography>
-                </Stack>
-                <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ pt: "1rem", pb: "1rem" }}>
-                  <Typography variant="titleMedium" sx={{ whiteSpace: "nowrap" }}>{"Bambini <14"}</Typography>
-                  <Typography variant="bodyMedium" sx={{ whiteSpace: "nowrap" }}>{!cityTaxResult.isFetching ? `${cityTaxResult.data?.children ?? 0} presenze` : <M3Skeleton width="4rem" />}</Typography>
-                </Stack>
-                <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ pt: "1rem", pb: "1rem" }}>
-                  <Typography variant="titleMedium" sx={{ whiteSpace: "nowrap" }}>{"Permanenze >10 giorni"}</Typography>
-                  <Typography variant="bodyMedium" sx={{ whiteSpace: "nowrap" }}>{!cityTaxResult.isFetching ? `${cityTaxResult.data?.over10Days ?? 0} presenze` : <M3Skeleton width="4rem" />}</Typography>
+          <Stack direction="row" sx={{
+            border: `1px solid ${theme.palette.outline.light}`,
+            borderRadius: "0.75rem"
+          }}>
+            <Stack spacing={2} sx={{
+              position: "relative",
+              justifyContent: "space-between",
+              p: "1rem",
+              borderRadius: "0.75rem",
+              color: theme.palette.onSurfaceVariant.light,
+            }}>
+              <Stack spacing={2}>
+                <Typography variant="headlineMedium">Tassa di soggiorno</Typography>
+                <Stack direction={drawerOpened ? "column" : "row"} spacing={1}>
+                  <M3DatePicker
+                    value={new Date(from)}
+                    onChange={(date: Date | null) => {
+                      if (date) {
+                        setIsFromValid(false);
+                        setFrom(Utils.dateToString(date));
+                      }
+                    }}
+                    onAccept={() => setIsFromValid(true)}
+                    onError={(reason) => setIsFromValid(reason === null)}
+                    shouldDisableDate={(date) => Utils.daysBetweenDates(to, Utils.dateToString(date)) > 0}
+                    renderInput={({ error, ...props }) => (
+                      <TextField
+                        {...props}
+                        label="Dal"
+                        error={error}
+                        helperText={error ? "Periodo non valido" : undefined}
+                        onBlur={() => setIsFromValid(!error)}
+                      />
+                    )}
+                  />
+                  <M3DatePicker
+                    value={new Date(to)}
+                    onChange={(date: Date | null) => {
+                      if (date) {
+                        setIsToValid(false);
+                        setTo(Utils.dateToString(date));
+                      }
+                    }}
+                    onAccept={() => setIsToValid(true)}
+                    onError={(reason) => setIsToValid(reason === null)}
+                    shouldDisableDate={(date) => Utils.daysBetweenDates(Utils.dateToString(date), from) > 0}
+                    renderInput={({ error, ...props }) => (
+                      <TextField
+                        {...props}
+                        label="Al"
+                        error={error}
+                        helperText={error ? "Periodo non valido" : undefined}
+                        onBlur={() => setIsToValid(!error)}
+                      />
+                    )}
+                  />
                 </Stack>
               </Stack>
-            </Collapse>
-          </Box>
+              <Stack direction="row" justifyContent="flex-end">
+                {cityTaxResult.isFetching ?
+                  <CircularProgress color="primary" /> :
+                  <M3TextButton onClick={() => {
+                    if (isCityTaxValid) {
+                      getCityTaxTrigger({ from, to });
+                    }
+                  }}>Calcola</M3TextButton>
+                }
+              </Stack>
+              <SurfaceTint sx={{
+                backgroundColor: theme.palette.primary.light,
+                opacity: theme.opacities.surface2,
+                m: "0 !important"
+              }}
+              />
+            </Stack>
+            <Box>
+              <Collapse in={openDetails} orientation="horizontal">
+                <Stack spacing={2} sx={{ p: "1rem" }}>
+                  <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ pt: "1rem", pb: "1rem" }}>
+                    <Typography variant="titleMedium" sx={{ whiteSpace: "nowrap" }}>Regolari</Typography>
+                    <Typography variant="bodyMedium" sx={{ whiteSpace: "nowrap" }}>{!cityTaxResult.isFetching ? `${cityTaxResult.data?.standard ?? 0} presenze` : <M3Skeleton width="4rem" />}</Typography>
+                  </Stack>
+                  <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ pt: "1rem", pb: "1rem" }}>
+                    <Typography variant="titleMedium" sx={{ whiteSpace: "nowrap" }}>{"Bambini <14"}</Typography>
+                    <Typography variant="bodyMedium" sx={{ whiteSpace: "nowrap" }}>{!cityTaxResult.isFetching ? `${cityTaxResult.data?.children ?? 0} presenze` : <M3Skeleton width="4rem" />}</Typography>
+                  </Stack>
+                  <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ pt: "1rem", pb: "1rem" }}>
+                    <Typography variant="titleMedium" sx={{ whiteSpace: "nowrap" }}>{"Permanenze >10 giorni"}</Typography>
+                    <Typography variant="bodyMedium" sx={{ whiteSpace: "nowrap" }}>{!cityTaxResult.isFetching ? `${cityTaxResult.data?.over10Days ?? 0} presenze` : <M3Skeleton width="4rem" />}</Typography>
+                  </Stack>
+                </Stack>
+              </Collapse>
+            </Box>
+          </Stack>
         </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 }
 
