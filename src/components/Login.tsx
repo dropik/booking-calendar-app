e6588e3cf059416ed7b1681ab0d4e6d894e1cdaf
Daniil Ryzhkov <daniil.ryzhkov@sarix.eu>
2023-09-17 18:21:28 +0200

    Added greeting on login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,6 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
+
+import { useNavigate } from "react-router-dom";
 
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
@@ -14,13 +16,25 @@ import M3Divider from "./m3/M3Divider";
 
 export default function Login(): JSX.Element {
   const theme = useTheme();
+  const navigate = useNavigate();
   const usernameRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
   const [auth, authResult] = api.endpoints.postAuthToken.useMutation();
+  const [getCurrentUserTrigger, currentUser] = api.endpoints.getCurrentUser.useLazyQuery();
 
-  if (authResult.isSuccess) {
-    window.location.href = "/";
-  }
+  useEffect(() => {
+    if (authResult.isSuccess) {
+      getCurrentUserTrigger(null);
+    }
+  }, [authResult.isSuccess, getCurrentUserTrigger]);
+
+  useEffect(() => {
+    if (currentUser.isSuccess) {
+      setTimeout(() => {
+        navigate("/app/table");
+      }, 1000);
+    }
+  }, [currentUser.isSuccess, navigate]);
 
   function submitForm(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault();
@@ -52,15 +66,44 @@ export default function Login(): JSX.Element {
           <Typography variant="displayMedium">Booking Calendar</Typography>
         </Stack>
         <M3Divider orientation="vertical" sx={{ height: "15rem" }} />
-        <Stack spacing={1} minWidth="26rem" pl="0.5rem" pr="2rem" sx={{ boxSizing: "border-box" }} component="form" onSubmit={submitForm}>
-          <Typography variant="headlineMedium" mb="1rem">Log-in</Typography>
-          <TextField fullWidth inputRef={usernameRef} name="username" label="Username" />
-          <TextField fullWidth inputRef={passwordRef} name="password" type="password" label="Password" />
-          <Stack direction="row" width="100%" justifyContent="space-between" alignItems="center">
-            <Typography variant="labelMedium" color={theme.palette.error.main}>{authResult.isError ? "Username o password sbagliati" : null}</Typography>
-            {authResult.isLoading ? <CircularProgress /> : <M3FilledButton type="submit">Log-in</M3FilledButton>}
+        <Box sx={{ position: "relative", minWidth: "26rem", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+          {!currentUser.isSuccess ? (
+            <Stack
+              spacing={1}
+              component="form"
+              onSubmit={submitForm}
+              sx={{
+                boxSizing: "border-box",
+                minWidth: "26rem",
+                pl: "0.5rem",
+                pr: "2rem",
+              }}>
+              <Typography variant="headlineMedium" mb="1rem">Log-in</Typography>
+              <TextField fullWidth inputRef={usernameRef} name="username" label="Username" />
+              <TextField fullWidth inputRef={passwordRef} name="password" type="password" label="Password" />
+              <Stack direction="row" width="100%" justifyContent="space-between" alignItems="center">
+                <Typography variant="labelMedium" color={theme.palette.error.main}>{authResult.isError ? "Username o password sbagliati" : null}</Typography>
+                {authResult.isLoading || currentUser.isFetching ? <CircularProgress /> : <M3FilledButton type="submit">Log-in</M3FilledButton>}
+              </Stack>
+            </Stack>
+          ) : null}
+          <Stack
+            direction="row"
+            spacing={2}
+            sx={{
+              position: "absolute",
+              justifyContent: "center",
+              alignItems: "center",
+              opacity: !currentUser.isSuccess ? 0 : 1,
+              transform: !currentUser.isSuccess ? "translateY(20px)" : undefined,
+              transition: theme.transitions.create(["opacity", "transform"], {
+                duration: theme.transitions.duration.standard,
+                easing: theme.transitions.easing.emphasized,
+              }),
+            }}>
+            <Typography variant="displaySmall">Ciao, {currentUser.data?.visibleName ?? currentUser.data?.username}!</Typography>
           </Stack>
-        </Stack>
+        </Box>
       </Stack>
     </Box>
   );
